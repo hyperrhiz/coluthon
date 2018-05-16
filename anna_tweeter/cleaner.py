4f3d7c3d06a0b64592c28c7ac3adb6ca080fec5f
hyperrhiz <hyperrhiz@gmail.com>
2018-05-16 17:48:04 -0400

    clean the cleaner's linefeeds

--- anna_tweeter/cleaner.py
+++ anna_tweeter/cleaner.py
@@ -1,1 +1,18 @@
-from unidecode import unidecodedef fuckfunnies(text):	return ''.join([i if ord(i) >= 32 and ord(i) < 127 else '' for i in text])def remove_non_ascii(text):    return unidecode(unicode(text, encoding = "utf-8"))                if __name__ == '__main__':		with open('citations.txt') as f:            text = f.read()        with open('cleaned.txt', 'a') as f:            f.write(remove_non_ascii(text))
+from unidecode import unidecode
+
+def fuckfunnies(text):
+	return ''.join([i if ord(i) >= 32 and ord(i) < 127 else '' for i in text])
+
+
+def remove_non_ascii(text):
+    return unidecode(unicode(text, encoding = "utf-8"))
+    
+    
+    
+    
+if __name__ == '__main__':
+	
+	with open('citations.txt') as f:
+            text = f.read()
+        with open('cleaned.txt', 'a') as f:
+            f.write(remove_non_ascii(text))
